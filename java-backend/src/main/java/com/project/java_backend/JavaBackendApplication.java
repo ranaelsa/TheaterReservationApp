d2b1fc217e61cdcc246062e8a5df5ca921291609
NathanD0 <122122847+nathanbrz@users.noreply.github.com>
2024-11-07 23:36:29 -0700

    Refresh git cache

--- java-backend/src/main/java/com/project/java_backend/JavaBackendApplication.java
+++ java-backend/src/main/java/com/project/java_backend/JavaBackendApplication.java
@@ -2,9 +2,8 @@ package com.project.java_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class JavaBackendApplication {
 
 	public static void main(String[] args) {
--- java-backend/src/main/java/com/project/java_backend/repository/UserRepository.java
+++ java-backend/src/main/java/com/project/java_backend/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.project.java_backend.repository;
 
+import org.springframework.stereotype.Repository;
+
 import com.project.java_backend.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Custom queries if needed
+    // Define any custom query methods here
 }
