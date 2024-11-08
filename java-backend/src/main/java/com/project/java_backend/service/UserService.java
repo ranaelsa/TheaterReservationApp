255550f16e432a2d197d4d70891405ff365abd33
NathanD0 <122122847+nathanbrz@users.noreply.github.com>
2024-11-07 22:37:57 -0700

    Set up basic controller, service, model, repository folders to work in later

---
+++ java-backend/src/main/java/com/project/java_backend/service/UserService.java
@@ -0,0 +1,24 @@
+package com.project.java_backend.service;
+
+import com.project.java_backend.model.User;
+import com.project.java_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Additional methods as needed
+}
