3d419714c80a3c3514dddcfe44950852c9d89ece
Destin Saba <129232125+destinsaba@users.noreply.github.com>
2024-11-09 16:20:51 -0700

    Set up basic emailservice

---
+++ java-backend/src/main/java/com/project/java_backend/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.project.java_backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.project.java_backend.service.EmailService;
+
+
+@RestController
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @Autowired
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/send-email")
+    public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
+        emailService.sendSimpleEmail(to, subject, text);
+        return "Email sent successfully";
+    }
+}
+
