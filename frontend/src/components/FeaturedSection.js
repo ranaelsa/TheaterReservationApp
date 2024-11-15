43cf5ffb10a2cfdb0b5e638b59457568c95565e8
Destin Saba <129232125+destinsaba@users.noreply.github.com>
2024-11-15 13:35:33 -0700

    Only show featured section to RUs

--- frontend/src/components/FeaturedSection.js
+++ frontend/src/components/FeaturedSection.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
@@ -5,9 +6,28 @@ import 'swiper/css/navigation';
 import MovieCard from './MovieCard';
 
 const FeaturedSection = ({ movies, theaters }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if userID is present in localStorage
+    const userID = localStorage.getItem('userID');
+    if (userID) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  if (!isLoggedIn) {
+    return (
+      <div className="flex flex-col items-center mt-8 pb-12">
+        <h1 className="text-5xl font-bold mb-4">Upcoming Releases</h1>
+        <p className="text-xl mb-6">Register now for only $20 per year to view upcoming releases and purchase tickets before public announcement.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center mt-8 pb-12">
-        <h1 className="text-5xl font-bold mb-4">Upcoming Releases</h1>
+      <h1 className="text-5xl font-bold mb-4">Upcoming Releases</h1>
       <p className="text-xl mb-6">As a registered user, you can buy tickets for these films before public announcement</p>
       <Swiper
         modules={[Navigation]}
